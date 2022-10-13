b5ca5b3c7125cb98cfd66f55ce04589d78f23878
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2022-10-13 12:46:59 +0200

    Przeniesienie projektu do IntelliJ IDEA

---
+++ src/main/java/pwo/Zadanie1.java
@@ -0,0 +1,16 @@
+package pwo;
+
+public class Zadanie1 {
+    public static final int FROM = 0, TO = 100;
+    public static final String FILE_NAME = "fibseq100.txt";
+
+    public static void main(String[] args) {
+
+        boolean ok = SequenceTools.writeToFile(new FibonacciGenerator(), FROM, TO, FILE_NAME);
+        if (ok) System.out.println("Wynik zapisany do pliku: "
+                + FILE_NAME);
+        else System.out.println("Błąd");
+        System.out.println("Fibonacci wersja 2");
+
+    }
+}
